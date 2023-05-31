8ab63ec0c84245bf1fb2ab9a3590677c6f825a6f
ShubhamPatel12499 <shubhampatel12499@gmail.com>
2023-05-31 23:36:10 +0530

    Product Backend

--- proxy-server.js
+++ proxy-server.js
@@ -1,20 +1,19 @@
 const express = require('express');
+const axios = require('axios');
 
 const app = express();
 
 app.get('/products', async (req, res) => {
   try {
-    const response = await fetch('https://services.odata.org/V2/Northwind/Northwind.svc/Products?$format=json');
-    const data = await response.json();
-    res.json(data);
+    const response = await axios.get('https://services.odata.org/V2/Northwind/Northwind.svc/Products?$format=json');
+    res.json(response.data);
   } catch (error) {
     console.error('Error fetching products:', error);
     res.status(500).json({ error: 'Failed to fetch products' });
   }
 });
 
-const PORT = 3001; 
+const PORT = 3001; // Choose a port number for your proxy server
 app.listen(PORT, () => {
   console.log(`Proxy server listening on port ${PORT}`);
 });
-
