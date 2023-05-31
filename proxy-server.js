b2ba836380344e11240e0ebaa04a165024290003
ShubhamPatel12499 <shubhampatel12499@gmail.com>
2023-05-31 23:19:04 +0530

    Product-Backend

---
+++ proxy-server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const app = express();
+
+app.use(cors({
+    origin:"*"
+}))
+
+app.get('/products', async (req, res) => {
+  try {
+    const response = await fetch('https://services.odata.org/V2/Northwind/Northwind.svc/Products?$format=json');
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    console.error('Error fetching products:', error);
+    res.status(500).json({ error: 'Failed to fetch products' });
+  }
+});
+
+const PORT = 3001; 
+app.listen(PORT, () => {
+  console.log(`Proxy server listening on port ${PORT}`);
+});
+
