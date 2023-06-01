1aa74511dafb0cee8d5a12e64d4590043fc9a546
ShubhamPatel12499 <shubhampatel12499@gmail.com>
2023-06-01 16:14:02 +0530

    Product Backend

--- proxy-server.js
+++ proxy-server.js
@@ -10,13 +10,9 @@ app.use(cors({
 
 app.get('/products', async (req, res) => {
   let page=req.query.$skiptoken;
-  let id=req.query.id;
-  if(!id){
-    id="";
-  }
- 
+  
   try {
-    await axios.get(`https://services.odata.org/V2/Northwind/Northwind.svc/Products(${id})?$format=json&$skiptoken=${page}`).then((r)=>{
+    await axios.get(`https://services.odata.org/V2/Northwind/Northwind.svc/Products?$format=json&$skiptoken=${page}`).then((r)=>{
         res.send(r.data)
     }).catch((err)=>{
       res.send("Error");
@@ -28,6 +24,24 @@ app.get('/products', async (req, res) => {
     res.status(500).json({ error: 'Failed to fetch products' });
   }
 });
+
+app.get("/products/:Id", async(req,res)=>{
+  const id=req.params.Id;
+  try {
+    await axios.get(`https://services.odata.org/V2/Northwind/Northwind.svc/Products(${id})?$format=json`).then((r)=>{
+        res.send(r.data)
+    }).catch((err)=>{
+      res.send("Error");
+    })
+   
+    
+  } catch (error) {
+    console.error('Error fetching products:', error);
+    res.status(500).json({ error: 'Failed to fetch products' });
+  }
+  
+})
+
 
 const PORT = 3001; // Choose a port number for your proxy server
 app.listen(PORT, () => {
